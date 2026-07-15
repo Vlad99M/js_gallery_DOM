a84a3fa696f826169f94abc9a7ad0938681dc1e0
w11ue <https://github.com/Vlad99M>
2026-07-15 23:59:21 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+document.getElementById('thumbs').addEventListener('click', function (e) {
+  const target = e.target;
+  const link = target.closest('a');
+
+  if (link) {
+    const largeImg = document.getElementById('largeImg');
+    const href = link.getAttribute('href');
+    const title = link.getAttribute('title');
+
+    largeImg.src = href;
+    largeImg.alt = title;
+  }
+});
